f1ea1d2c5d11e8b150519a3919da3209b84d385b
abdullahghausliqteq <abdullahghaus0440@gmail.com>
2025-01-02 22:52:48 +0500

    vercel

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const { default: mongoose } = require("mongoose")
+
+const app = express()
+
+
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+const app = require("./app")
+const cors = require("cors")
+
+app.use(cors())
+
+try {
+    mongoose
+        .connect('mongodb+srv://abdullahghaus:Power2Abdullah@ecommercelearning.3l0e6hu.mongodb.net/?retryWrites=true&w=majority&appName=EcommerceLearning')
+        .then(inst => console.log("Mongoose Connected"))
+} catch (err) {
+    console.log(err)
+}
+
+app.use("/api/v1/isWorking", (req, res) => {
+    res.send("Server is working fine")
+})
+
+app.listen(6000, () => {
+    console.log("Server Connected")
+})
