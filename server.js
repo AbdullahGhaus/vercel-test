200748702a229d3ce3d40b7fc16c5fb87e62b602
Muhammad Abdullah Ghaus <90969195+AbdullahGhaus@users.noreply.github.com>
2025-01-03 15:12:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const mongoose = require("mongoose")
-const app = require("./app")
-const cors = require("cors")
+// const mongoose = require("mongoose")
+// const app = require("./app")
+// const cors = require("cors")
 
-app.use(cors())
+// app.use(cors())
 
-try {
-    mongoose
-        .connect('mongodb+srv://abdullahghaus:Power2Abdullah@ecommercelearning.3l0e6hu.mongodb.net/?retryWrites=true&w=majority&appName=EcommerceLearning')
-        .then(inst => console.log("Mongoose Connected"))
-} catch (err) {
-    console.log(err)
-}
+// try {
+//     mongoose
+//         .connect('mongodb+srv://abdullahghaus:Power2Abdullah@ecommercelearning.3l0e6hu.mongodb.net/?retryWrites=true&w=majority&appName=EcommerceLearning')
+//         .then(inst => console.log("Mongoose Connected"))
+// } catch (err) {
+//     console.log(err)
+// }
 
-app.use("/api/v1/isWorking", (req, res) => {
-    res.send("Server is working fine")
-})
+// app.use("/api/v1/isWorking", (req, res) => {
+//     res.send("Server is working fine")
+// })
 
-app.listen(6000, () => {
-    console.log("Server Connected")
-})
+// app.listen(6000, () => {
+//     console.log("Server Connected")
+// })
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
